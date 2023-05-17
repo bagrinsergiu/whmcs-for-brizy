c3d35a67f50f3271d7736f95dd7a6433bed0e6ac
Zaharia Alexandru <alecszaharia@gmail.com>
2023-05-17 09:29:17 +0300

    fix: moved npm install and npm run build:prod in workflow

--- bin/build.sh
+++ bin/build.sh
@@ -7,24 +7,14 @@ RELEASE_NAME="${WHMCS_NAME}_${WHMCS_PLUGIN_VERSION}"
 TARGET_DIR="${1:-/tmp/build}"
 BUILD_NAME="${2:-$RELEASE_NAME}"
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
-MODULE_DIR="../whmcs-addon"
-ADMIN_DIR="../brizy-admin"
 PARENT_DIR="${SCRIPT_DIR}/.."
-
-
-echo "CREATING RELEASE: ${BUILD_NAME} -> ${TARGET_DIR}'"
-
-(cd $ADMIN_DIR && rm -rf node_modules && npm install --force)
-npm run build:prod
+MODULE_DIR="${PARENT_DIR}/whmcs-addon"
 
 mkdir -p ${TARGET_DIR}
 
-# cleanup all files that are not needed
-rm -rf ${TARGET_DIR}/.git
-rm -rf ${TARGET_DIR}/.idea
-# ...
-
+echo "CREATING RELEASE: ${BUILD_NAME} -> ${TARGET_DIR}'"
 cp -R ${MODULE_DIR}/. ${TARGET_DIR}/module_data/
+
 (cd ${TARGET_DIR}/module_data/ && zip -r ${TARGET_DIR}/${BUILD_NAME}.zip .)
 
 rm -rf ${TARGET_DIR}/module_data
