4f2d672b7d273a4e4215232310b8a74c33c453a5
mix <t.wroblewski@mserwis.pl>
2023-05-17 15:23:01 +0200

    TEST BUILD

--- whmcs-addon/modules/addons/brizy/brizy.php
+++ whmcs-addon/modules/addons/brizy/brizy.php
@@ -21,7 +21,7 @@ function brizy_config()
         'description' => 'Brizy for WHMCS extends the capabilities of WHMCS to remotely install WordPress, Brizy or the Brizy PRO version on shared hosting accounts. Both when creating new orders and using a specially prepared installer for existing customers.',
         'author' => 'DOTINUM.COM',
         'language' => 'english',
-        'version' => '1.0',
+        'version' => '1.01',
         'fields' => [
             'product_name' => [
                 'FriendlyName' => 'Product names',
