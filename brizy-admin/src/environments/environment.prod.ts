7932ae08cc2f34305d493c03207076a3bf2baaf5
mix <t.wroblewski@mserwis.pl>
2023-07-13 12:01:05 +0200

    Fix for loading language files (JS)

--- brizy-admin/src/environments/environment.prod.ts
+++ brizy-admin/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
+const whmcsPath = window.location.href.substring(0, window.location.href.indexOf("/admin/")) || window.location.href.substring(0, window.location.href.indexOf("/clientarea.php"));
 export const environment = {
     production: true,
-    i18nPath: '/modules/addons/brizy/lang/js'
+    i18nPath: whmcsPath + '/modules/addons/brizy/lang/js'
 };
