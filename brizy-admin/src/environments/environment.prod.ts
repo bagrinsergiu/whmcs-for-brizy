0876c7c99fc5fdc1aefecbb5961fc5b0c76b3776
mix <t.wroblewski@mserwis.pl>
2023-10-19 10:41:51 +0200

    fix: improved loading of language files (JS)

--- brizy-admin/src/environments/environment.prod.ts
+++ brizy-admin/src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
-const whmcsPath = window.location.href.substring(0, window.location.href.indexOf("/admin/"))
+declare var whmcsBaseUrl: string;
+
+let whmcsPath =  window.location.href.substring(0, window.location.href.indexOf("/admin/"))
 || window.location.href.substring(0, window.location.href.indexOf("/clientarea.php"))
 || window.location.href.substring(0, window.location.href.indexOf("/index.php"));
+
+if (!whmcsPath && typeof whmcsBaseUrl !== 'undefined') {
+    whmcsPath = window.location.protocol + '//' + window.location.host + whmcsBaseUrl;
+}
 
 export const environment = {
     production: true,
